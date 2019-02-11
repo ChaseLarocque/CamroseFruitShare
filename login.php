6e7f8c7787b9a639c1839742960055e6bb6b952a
AlexRHo <32780777+AlexRHo@users.noreply.github.com>
2019-02-10 21:00:53 -0700

    Create Index & Login
    created the login page and the index page.
    Did some CSS to give it some borders and color that will give an idea as to how the web page will look.

--- login.php
+++ login.php
@@ -17,7 +17,7 @@ will be available to all users.
 <!doctype html>
 <html class="no-js" lang="en" dir="ltr">
 
- <head>
+  <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -28,44 +28,40 @@ will be available to all users.
     <link rel="stylesheet" href="resources/css/index.css">
 
 
-    <title>Hidden Harvest of Camrose</title>
- </head>
- 
- <body>
+
+    <title>Login Fruit Pickers</title>
+  </head>
+  <body>
   	<div class="header">
 	    <h1>
 	    <a href="index.php">Hidden Harvests of Camrose</a>
 		</h1>
 	</div>
 
-	<div class="body">
-		<div class="login">
-			<h1>
-				<a href="login.php">Login</a>
+	
 
-			</h1>
-		</div>
+
+
+  <div class="footer">
+      <div class="row footerSpace">
+        <div class="large-4 medium-4 small-4 columns picture">Photo</div>
+        <div class="large-4 medium-4 small-4 columns dev">
+          <p>
+            Designed February 01,2019<br>
+            by Alex Ho, Chase Larocque, Justin Ikenouye.
+          </p>
+          </div>
+          <div class="large-4 medium-4 small-4 columns contactInfo">
+            <h3 class="underline">Contact Information</h3>
+          </div>
+      </div>
 	</div>
 
-	<div class="footer">
-	    <div class="row footerSpace">
-	      <div class="large-3 medium-3 small-3 columns picture">Photo</div>
-	      <div class="large-3 medium-3 small-3 columns dev">
-	        <p>
-	          Designed February 01,2019<br>
-	          by Alex Ho, Chase Larocque, Justin Ikenouye.
-	        </p>
-	        </div>
-	        <div class="large-3 medium-3 small-3 columns contactInfo">
-	          <h3 class="underline">Contact Information</h3>
-	        </div>
-	    </div>
-	</div>
-</body>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  </body>
 </html>
