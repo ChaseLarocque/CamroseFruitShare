dd46403afb7457561ed48fc62c8adf12d647e6e8
JustinIkenouye <32780771+JustinIkenouye@users.noreply.github.com>
2019-03-10 16:27:55 -0600

    Phone scaling for header
    Still not great for xs screens, title gets too small.

--- index.php
+++ index.php
@@ -33,13 +33,13 @@ will be available to all users.
     <body>
         <div class = "main-container">
         	<div class="header row">
-        		<div class="col-3"></div>
-        		<div class="col-6 my-auto">
+        		<div class="col-xs-0 col-sm-2"></div>
+        		<div class="col-xs-12 col-sm-8 my-auto">
         	    	<h1 id = "title">
         	           Hidden Harvests of Camrose
         	     	</h1>
         	    </div>
-        		<div class="col-3 logButton my-auto">
+        		<div class="col-xs-12 col-sm-2 logButton my-auto">
         			<h4>
         				<a href="login.php" class="btn btn-secondary" role="button">Login</a>
         			</h4>
