c4e397cb26416aa7060ab39273a1a25a8c689e7d
AlexRHo <32780777+AlexRHo@users.noreply.github.com>
2019-03-17 20:00:17 -0600

    Connected DB to fruitOffer.PHP
    fruit offering is now connected to the DB
    Offering is now submitted into the DB

--- DBConnect.php
+++ DBConnect.php
@@ -2,7 +2,7 @@
 //DBConnect.php
 //Contains data to connect to database using PDO
 
-define('DB_SERVER', 'localhost:3306');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'fruitPickers');
