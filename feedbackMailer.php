bf644249035b2f600d709c58e944791f1e8919e8
JustinIkenouye <32780771+JustinIkenouye@users.noreply.github.com>
2019-04-02 14:24:54 -0600

    Delete unnecessary code

--- feedbackMailer.php
+++ feedbackMailer.php
@@ -1,14 +1,14 @@
 <!--
 feedbackMailer.php
 
-Contains code to mail the feedback to Greg (Or whoever is specified)
-Uses swift mailer https://swiftmailer.symfony.com/docs/messages.html
+Contains code to mail the feedback to Greg (Or whoever is specified in mail())
 -->
 <?php
 if($_POST["feedback"]){
-	$msg = wordwrap($_POST["feedback"],70);
-	$result = mail("jikenouy@ualberta.ca", "Feedback Form", $msg, "From: feedback@csl.augustana.ualberta.ca");
+	$msg = wordwrap($_POST["feedback"],70); //wrap text to fit in email
+	$result = mail('jikenouy@ualberta.ca', 'Feedback Form', $msg, 'From: feedback@csl.augustana.ualberta.ca');
 
+	//FOR ERROR CHECKING. REMOVE WHEN THIS WORKS
 	if($result){
 		echo "SENT";
 	}
@@ -16,17 +16,4 @@ if($_POST["feedback"]){
 		echo "Failed";
 	}
 }
-
-// $ composer require "swiftmailer/swiftmailer:^6.0"
-// require_once '/path/to/vendor/autoload.php';
-
-// $transport = (new swiftmailer)
-
-
-// //create a message
-// $message = (new Swift_Message('Hidden Harvests Feedback'))
-// 	->setFrom(['currentuser'])
-
-// //send the message
-// $result = $mailer->send($message);
 ?>
