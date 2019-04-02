79717b8698c03e511e55b1754a8288e3e7055e36
AlexRHo <32780777+AlexRHo@users.noreply.github.com>
2019-04-01 22:49:06 -0600

    Document deleteSubmission.php
    Document code that is written to ensure understanding of PHP code

--- deleteSubmission.php
+++ deleteSubmission.php
@@ -1,34 +1,48 @@
 <?php 
-/*
-function deleteOffer($offerId){
-	require 'DBConnect.php';
-	$sql = $pdo->prepare( "DELETE FROM fruit_offer 
-		WHERE  offerId = ?");
-	$sql -> bindParam(1, $offerId, PDO::PARAM_STR, 50);	
-	$sql -> execute();
-	$pdo = null;
-	$sql = null;
-	header("location: userPage.php");
-}*/
-if($_REQUEST['action'] == 'deleteOffer'){
+/**
+AUCSC401 - Hidden Harvests of Camrose (Camrose Fruit picking website)
+
+March 31, 2019
+
+deleteSubmission.php
+
+This php file contains the set of code that will execute an SQL query to delete a row from
+the fruit_offer or fruit_request code
+**/
+
+/*------------------------------------------------------------------------------------------*/
+
+require 'DBConnect.php'; //Connect to the DB
+
+if($_REQUEST['action'] == 'deleteOffer'){ //Check if action is deleteOffer from ajax
+
 	$offerId = $_REQUEST['offerId'];
 
-	require 'DBConnect.php';
 	$sql = $pdo->prepare( "DELETE FROM fruit_offer 
-		WHERE  offerId = ?");
-	$sql -> bindParam(1, $offerId, PDO::PARAM_STR, 50);	
+		WHERE  offerId = ?"); // Prepare the SQL Query 
+
+	$sql -> bindParam(1, $offerId, PDO::PARAM_STR, 50); // Bind Parameter $offerId to STR(50) (more secure)
+
 	$sql -> execute();
-	$pdo = null;
+
+	$pdo = null; //Set $pdo & $sql to null (more secure)
 	$sql = null;
-	}
-if($_REQUEST['action'] == 'deleteRequest'){
+
+	}// if
+
+if($_REQUEST['action'] == 'deleteRequest'){ //Check if action is deleteRequest from ajax
+
 	$requestId = $_REQUEST['requestId'];
-	require 'DBConnect.php';
+
 	$sql = $pdo->prepare( "DELETE FROM fruit_request 
-		WHERE  requestId = ?");
-	$sql -> bindParam(1, $requestId, PDO::PARAM_STR, 50);	
+		WHERE  requestId = ?"); //preparing sql statement to execute
+
+	$sql -> bindParam(1, $requestId, PDO::PARAM_STR, 50); //Bind parameter $requestId to STR(50) (More Secure)
+
 	$sql -> execute();
-	$pdo = null;
+
+	$pdo = null; //Set $pdo & $sql to null (more secure)
 	$sql = null;
-	}
+
+	}//if
 ?>
--- resources/js/deleteButtonFunc.js
+++ resources/js/deleteButtonFunc.js
@@ -6,42 +6,34 @@ March 31, 2019
 deleteButtonFunc.js
 
 This JavaScript file holds the different javascript files that
-will be handling the delete button on the user page.
+will be handling the delete and confirm button on the user page display tables.
 **/
+
+/*------------------------------------------------------------------------------------------*/
 
 /** 
 This function is called when the Delete button is clicked
-on the userPage within the Offer display table.
-It will change the button into a confirm button which will
+on the userPage within the Offer display table or request display table.
+It will change the delete button into a confirm button which will
 need to be pressed again in order to confirm deleting the 
 submission. 
 **/
 
 function confirmOfferDelete(id){
   var buttonId = document.getElementById(id); // Grabs the element from the html that has an id == id
-  buttonId.innerText = 'CONFIRM'; // Change the text of the button to Confirm
-  //buttonId.className = 'btn btn-secondary btnRed';
-  buttonId.setAttribute("onclick", 'deleteOffer(id)');
+  buttonId.innerText = 'CONFIRM'; // Change button text
+  buttonId.setAttribute("onClick", 'deleteOffer(id)');// Change onClick call new function
 }
-
-/** 
-This function is called when the Delete button is clicked
-on the userPage within the request display table.
-It will change the button into a confirm button which will
-need to be pressed again in order to confirm deleting the 
-submission. 
-**/
 
 function confirmRequestDelete(id){
   var buttonId = document.getElementById(id);
-  buttonId.innerText = 'CONFIRM';
-  //buttonId.className = 'btn btn-secondary btnRed';
-  buttonId.setAttribute("onclick", 'deleteRequest(id)');
+  buttonId.innerText = 'CONFIRM'; //Change button text
+  buttonId.setAttribute("onClick", 'deleteRequest(id)'); //Change onClick call new function
 }
 
 /**
-Once the Confirm button is clicked on the offer table display,
-this function will be called to then make an ajax request to the 
+Once the Confirm button is clicked on the offer table display or request table display,
+these functions will be called to then make an ajax request to the 
 deleteSubmission.php file that will execute server side code 
 in order to delete submission from the Database.
 Once delete is finished it will refresh the page.
@@ -49,26 +41,19 @@ Once delete is finished it will refresh the page.
 
 function deleteOffer(id){// id is the button id which is the offerId on the fruit_offer table in the DB
   $.ajax({
-    type: "POST",
-    url: 'deleteSubmission.php',
-    data: {action: 'deleteOffer', offerId: id}
+    type: "POST", //Request Type
+    url: 'deleteSubmission.php', //Post to File
+    data: {action: 'deleteOffer', offerId: id} //Pass data along
   });
-  window.location.href=window.location.href;
+  window.location.href=window.location.href; // Reload page after complete
 }
 
 
-/**
-Once the Confirm button is clicked on the request table display,
-this function will be called to then make an ajax request to the 
-deleteSubmission.php file that will execute server side code 
-in order to delete submission from the Database.
-Once delete is finished it will refresh the page.
-**/
 function deleteRequest(id){ // id is the button id which is the requestId on the fruit_request table in the DB
   $.ajax({
-    type: "POST",
-    url: 'deleteSubmission.php',
-    data: {action: 'deleteRequest', requestId: id}
+    type: "POST", //Request Type
+    url: 'deleteSubmission.php', //Post to file
+    data: {action: 'deleteRequest', requestId: id} //Pass data along
   });
-  window.location.href=window.location.href;
+  window.location.href=window.location.href; // Reload page after complete
 }
