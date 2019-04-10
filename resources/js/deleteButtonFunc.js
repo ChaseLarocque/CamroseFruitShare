05220b5bcf322fc3d829716f4a9b9c0b2f8849c5
ChaseLarocque <32780762+ChaseLarocque@users.noreply.github.com>
2019-04-10 11:43:57 -0600

    changed back to Alex's change
    I pushed an change to Dev that overwrote Alex's work. This fixes that.

--- resources/js/deleteButtonFunc.js
+++ resources/js/deleteButtonFunc.js
@@ -3,8 +3,6 @@ deleteButtonFunc.js
 Alex Ho, Chase Larocque, Justin Ikenouye
 AUCSC401 - Hidden Harvests of Camrose (Camrose Fruit picking website)
 March 31, 2019
-
-
 This JavaScript file holds the different javascript files that
 will be handling the delete and confirm button on the user page display tables.
 **/
@@ -23,17 +21,16 @@ function confirmOfferDelete(id){
   originalId = id;
   var buttonId = document.getElementById('delete'+id); // Grabs the element from the html that has an id == id
   buttonId.innerText = 'CONFIRM'; // Change button text
-  buttonId.setAttribute("onClick", 'deleteOffer(id)');// Change onClick call new function
-}//confirmOfferDate
-
+  buttonId.setAttribute("onClick", 'deleteOffer(originalId)');// Change onClick call new function
+  console.log(id);
+}
 
 function confirmRequestDelete(id){
   originalId = id;
   var buttonId = document.getElementById('delete'+id);
   buttonId.innerText = 'CONFIRM'; //Change button text
-  buttonId.setAttribute("onClick", 'deleteRequest(id)'); //Change onClick call new function
-}//confirmRequestDate
-
+  buttonId.setAttribute("onClick", 'deleteRequest(originalId)'); //Change onClick call new function
+}
 
 /**
 Once the Confirm button is clicked on the offer table display or request table display,
@@ -48,21 +45,21 @@ function deleteOffer(id){// id is the button id which is the offerId on the frui
   console.log(id);
   $.ajax({
     url: 'deleteSubmission.php', //Post to File
-    data: {action: 'deleteOffer', offerId: id, //Pass data along
-    success: waitTimer()}
+    data: {action: 'deleteOffer', offerId: id}, //Pass data along
+    success: waitTimer(),
     type: 'POST' //Request Type
   });
-}//deleteOffer
+}
 
 function deleteRequest(id){ // id is the button id which is the requestId on the fruit_request table in the DB
   action = 'deleteRequest';
   $.ajax({
     url: 'deleteSubmission.php', //Post to file
-    data: {action: 'deleteRequest', requestId: id, //Pass data along
-    success: waitTimer()}
+    data: {action: 'deleteRequest', requestId: id}, //Pass data along
+    success: waitTimer(),
     type: 'POST' //Request Type
   });
-}//deleteRequest
+}
 
 /**
 This function waits long enough for the SQL querry to finish
